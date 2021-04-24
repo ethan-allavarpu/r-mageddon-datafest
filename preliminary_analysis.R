6ef3de6ecaa60c2fbe480b73324c1708bee98c6d
Dara Tan <daratanxe@Daras-MacBook-Pro.local>
2021-04-24 22:24:58 +0800

    added nmu by alcohol/tobacco use; relationship between nmu and dast-10

--- preliminary_analysis.R
+++ preliminary_analysis.R
@@ -3,7 +3,7 @@ library(tidyverse)
 us18 <- read.csv("US/us_18Q1.csv") %>%
   select(-DATE, -QTIME, -START_DATE, -STATUS)
 
-# QUESTION: DOES SECTION ORDER MATTER? ----
+# INFLUENCE OF SECTION ORDER ----
   # potentially insightful for questionnaire design
   # could run a bootstrap hypothesis test
 
@@ -106,7 +106,7 @@ college_nmu %>%
 
   # Q14: mean of 4 (online university) is only a bit smaller than 1 (two-year)
     # or 2 (four-year) at ~0.7-0.9; 5 (trade school) is clearly largest at ~2.5
-    # 4 also has largest variance -> likely not all students engage in nmu,
+    # 4 also has largest variance -> likely not all students engage in NMU,
     # but those who do, consume multiple drugs
 college_nmu %>%
   group_by(COLLEGE_LENGTH) %>%
@@ -133,7 +133,7 @@ college_nmu %>%
 
 # MENTAL ILLNESS AND NMU ----
 ments <- str_subset(names(us18), "MENT_")
-ments <- ments[1:(length(ments) - 2)] # drop other and none
+ments <- ments[1:(length(ments) - 2)] # drop OTH (other) and NONE
 ment_nmu_summary <- function(ment_column_name) {
   ment_totals <- summary(as.factor(us18[[ment_column_name]]))
   ment_nmu_df <- us18 %>%
@@ -168,4 +168,52 @@ names(ment_nmu) <- ments
     # PTSD: 1 > 0 for all drugs except SUF and TAP; by ~2+ times mostly
     # SCH: 1 > 0 for all drugs; by up to >3 times
 
-# RELATIONSHIP BETWEEN NMU AND DAST-10 (VARY BY DRUG TYPE?) -- NEXT
+# ALCOHOL/TOBACCO USE ----
+  # clear jump from 1 (0 drinks/week) and 2 (1-7) to 3 (8-14) and 4 (15-21)
+  # 5 (22 and above) above 3 and 4 for some drugs but below for others
+us18 %>%
+  select(ALC_USE, nmus) %>%
+  mutate_all(~replace(., is.na(.), 0)) %>%
+  group_by(ALC_USE) %>%
+  summarize(fent = mean(FENT_NMU), bup = mean(BUP_NMU), meth = mean(METH_NMU),
+            morph = mean(MORPH_NMU), oxy = mean(OXY_NMU),
+            oxym = mean(OXYM_NMU), tram = mean(TRAM_NMU), tap = mean(TAP_NMU),
+            hyd = mean(HYD_NMU), hydm = mean(HYDM_NMU), suf = mean(SUF_NMU),
+            cod = mean(COD_NMU), dihy = mean(DIHY_NMU), benz = mean(BENZ_NMU),
+            stim = mean(STIM_NMU), thc = mean(THC_NMU), ktm = mean(KTM_NMU))
+
+  # 1 (currently use) is higher than 2 (never) and 3 (previously used) for all
+  # drugs; 3 is up to ~2 times of 2 for some drugs but comparable on others
+us18 %>%
+  select(TOB_USE, nmus) %>%
+  mutate_all(~replace(., is.na(.), 0)) %>%
+  group_by(TOB_USE) %>%
+  summarize(fent = mean(FENT_NMU), bup = mean(BUP_NMU), meth = mean(METH_NMU),
+            morph = mean(MORPH_NMU), oxy = mean(OXY_NMU),
+            oxym = mean(OXYM_NMU), tram = mean(TRAM_NMU), tap = mean(TAP_NMU),
+            hyd = mean(HYD_NMU), hydm = mean(HYDM_NMU), suf = mean(SUF_NMU),
+            cod = mean(COD_NMU), dihy = mean(DIHY_NMU), benz = mean(BENZ_NMU),
+            stim = mean(STIM_NMU), thc = mean(THC_NMU), ktm = mean(KTM_NMU)) %>% View()
+
+# RELATIONSHIP BETWEEN NMU AND DAST-10 ----
+dasts <- str_c("DAST_", 1:10)
+dast_props <- us18 %>%
+  select(dasts) %>%
+  mutate(DAST_3 = ifelse(DAST_3 == 1, 0, 1)) %>% # 1 is desirable for DAST_3
+  colMeans()
+nmu_dast_summary <- function(nmu_column_name) {
+  nmu_column <- us18[[nmu_column_name]]
+  nmu_column[is.na(nmu_column)] <- 0
+  nmu_dast_sums <- vapply(dasts, function(x) {
+    sum(us18[[x]] & nmu_column, na.rm = TRUE)
+  }, numeric(1))
+  nmu_dast <- (nmu_dast_sums / sum(nmu_column, na.rm = TRUE)) / dast_props
+  as.data.frame(as.list(nmu_dast))
+}
+nmu_dast <- map_dfr(nmus, nmu_dast_summary)
+rownames(nmu_dast) <- nmus
+  # eye-balling observations:
+    # does not appear to be consistent ordering across DAST questions, but
+    # 12 (COD) is lowest in all but DAST_1 and DAST_3
+    # 3 (METH) is highest most frequently -> five out of ten questions
+    # suggests that DAST is better at identifying NMU of some drugs than others
